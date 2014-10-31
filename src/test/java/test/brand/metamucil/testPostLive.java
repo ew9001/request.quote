978c05e783d6c9246e8b0993310e30cd8c221647
Earl Wilis <earl.willis@publicismodem.com>
2014-10-31 11:21:02 -0400

    test

--- src/test/java/test/brand/metamucil/testPostLive.java
+++ src/test/java/test/brand/metamucil/testPostLive.java
@@ -52,7 +52,7 @@ public class testPostLive {
 	    String safe = "http://10.10.10.34:8080/job/TESTNG3/ws/PageLoads";
 	  @Test(groups = {"create"})
 		@Parameters({"browser"})
-	  @BeforeClass
+	//  @BeforeClass
 	  public void beforeClass(String browser) throws IOException, InterruptedException
 	  {	   
 		  
